4e71ed5fcef314faa8e0a788bf4ef8890bdd678a
Sam Kopp <github@snxwman.dev>
2024-07-27 16:12:05 -0400

    leetcode - 125. Valid Palindrome (more solutions)

--- leetcode/125. Valid Palindrome/solution.rs
+++ leetcode/125. Valid Palindrome/solution.rs
@@ -10,4 +10,49 @@ impl Solution {
 
         s_forwards == s_backwards
     }
+
+    pub fn is_palindrome2(s: String) -> bool {
+        let s: String = s
+            .to_lowercase()
+            .chars()
+            .filter(|c| c.is_ascii_alphanumeric())
+            .collect();
+
+        for (c1, c2) in s.chars().zip(s.chars().rev()) {
+            if c1 != c2 {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    pub fn is_palindrome3(s: String) -> bool {
+        s.to_lowercase()
+            .chars()
+            .filter(|c| c.is_ascii_alphanumeric())
+            .zip(
+                s.to_lowercase()
+                    .chars()
+                    .filter(|c| c.is_ascii_alphanumeric())
+                    .rev()
+            )
+            .map(|(c1, c2)| {
+                if c1 != c2 {
+                    return false;
+                }
+                true
+            })
+            .all(|i| i == true)
+    }
+
+    pub fn is_palindrome4(s: String) -> bool {
+        let s: String = s
+            .to_lowercase()
+            .chars()
+            .filter(|c| c.is_ascii_alphanumeric())
+            .collect();
+
+        s.chars().eq(s.chars().rev())
+    }
 }
